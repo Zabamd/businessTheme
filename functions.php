92874881a42ac78ede5d913add84bac0bac298fa
zabierowski <zabierowski@nomachine.com>
2023-01-03 16:29:25 +0100

    wins'n'losses

---
+++ functions.php
@@ -0,0 +1,30 @@
+<?php
+
+class BusinessTheme
+{
+    public function __construct() 
+    {
+        add_action( 'after_setup_theme', [$this, 'themeSetup'] );
+        add_action( 'wp_enqueue_scripts', [$this,'enqueueStyle'] );
+        add_action( 'init', [$this, 'patterCategory'] );
+    }
+
+    public function themeSetup() : void
+    {
+        add_editor_style( 'style.css' );
+        add_theme_support( 'wp-block-styles' );
+    }
+
+    public function enqueueStyle() : void
+    {
+        wp_enqueue_style("main-style",get_stylesheet_uri());
+    }
+    
+    public function patterCategory() : void 
+    {
+        register_block_pattern_category( 'BusinessTheme', array('label' => __('BusinessTheme'))); 
+    }
+}
+
+$init = new BusinessTheme();
+?>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.{html,js,php}",
+    "./parts/**/*.{html,js,php}",
+    "./patterns/**/*.{html,js,php}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
